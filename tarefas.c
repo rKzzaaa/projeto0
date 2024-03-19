461b030a2d83562596f3cd6b9321925c8e83e936
RAFAEL DENIS ALONSO <unifralonso@feinet.fei.edu.br>
2024-03-19 20:54:15 -0300

    add tarefas

---
+++ tarefas.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "tarefas.h"
+
+int criar(Tarefa tarefas[]int *pos) {
+    printf("funcao de criar tarefas\n");
+    return 0;
+
+}
+
+int deletar(Tarefa tarefas[], int *pos) {
+    printf("funcao de deletar tarefas\n");
+    return 0;
+
+}
+
+int listar(Tarefa tarefas[], int pos) {
+    printf("funcao de listar tarefas\n")
+    return 0;
+
+}
+
+int salvar(Tarefa tarefas[], int total, int pos){
+    printf("funcao de salvar tarefas\n");
+    return 0;
+
+}
+
+int carregar(Tarefa tarefas[], int total, int pos) {
+    printf("funcao de carregar tarefas")
+    return 0;
+}
+
+
+
+
