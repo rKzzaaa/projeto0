4affb1346d55889e0422768ef84f5555670a026a
RAFAEL DENIS ALONSO <unifralonso@feinet.fei.edu.br>
2024-03-19 20:16:27 -0300

    menu principal

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main(){
     int opcao;
-    printf("hello, world");
+    do {
+        printf("\nMenu principal\n");
+        printf("1 - Criar tarefa \n");
+        printf("2 - Deletar tarefa\n");
+        printf("3 - Listar tarefas\n");
+        printf("0 - sair\n");
+        printf("Entrar com uma opcao\n");
+        int i = scanf("%d\n",&opcao);
+        printf("Opcao escolhida: %d\n", opcao);
+    } while(opcao != 0);
 }
+
