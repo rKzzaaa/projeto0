de04dd73752518da7acaef02d31f4c7b750cdfbf
rKzzaaa <144257364+rKzzaaa@users.noreply.github.com>
2024-03-26 23:19:22 +0000

    fizemos os arquivos binarios

--- main.c
+++ main.c
@@ -1,39 +1,33 @@
-#include <stdio.h>
+
 #include "tarefas.h"
-int main(){
-    int opcao;
-    int pos =0;
-    Tarefa tarefas [TOTAL];
-    do {
-        printf("\nMenu principal\n");
-        printf("1 - Criar tarefa \n");
-        printf("2 - Deletar tarefa\n");
-        printf("3 - Listar tarefas\n");
-        printf("0 - sair\n");
-        printf("Entrar com uma opcao\n");
-        int i = scanf("%d\n",&opcao);
-        printf("Opcao escolhida: %d\n", opcao);
-        if (opcao == 1 ){
-            int erro = criar(tarefas, &pos);
+#include <stdio.h>
 
-        }
-        else if (opcao == 2) {
-            int erro = deletar(tarefas, &pos);
-        }
+int main() {
+  int pos = 0;
+  Tarefa tarefas[TOTAL];
 
-        else if (opcao == 3 ){
-            int erro = listar(tarefas, pos);
-        }
+  int opcao;
+  do {
+    printf("\nMenu principal\n");
+    printf("1 - Criar tarefa\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefas\n");
+    printf("0 - Sair\n");
+    printf("Entre com uma opcao: ");
+    int i = scanf("%d", &opcao);
 
-        else if (opcao == 0 ){
-            printf("Sair\n");
-        }
+    printf("Opcao escolhida: %d\n", opcao);
+    if (opcao == 1) {
+      int erro = criar(tarefas, &pos);
+    } else if (opcao == 2) {
+      int erro = deletar(tarefas, &pos);
+    } else if (opcao == 3) {
+      int erro = listar(tarefas, pos);
+    } else if (opcao == 0) {
+      printf("Sair\n");
+    } else {
+      printf("opcao invalida");
+    }
 
-        else {
-            printf("opcao invalida");
-        }
-
-    } while(opcao != 0);
-
-    
+  } while (opcao != 0);
 }
