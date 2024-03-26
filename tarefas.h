993863cc69bc22d6f8434a86f3c95bee2aedb08f
rKzzaaa <144257364+rKzzaaa@users.noreply.github.com>
2024-03-26 22:25:18 +0000

    fizemos a função de deletar tarefas

--- tarefas.h
+++ tarefas.h
@@ -1,13 +1,18 @@
+
 #define TOTAL 100
 
-typedef struct Tarefa{
-int prioridade;
-char descricao[300];
-char categoria[100];
-}Tarefa;
+typedef struct Tarefa {
+  int prioridade;
+  char descricao[300];
+  char categoria[100];
+} Tarefa;
 
 int criar(Tarefa tarefas[], int *pos);
 int deletar(Tarefa tarefas[], int *pos);
 int listar(Tarefa tarefas[], int pos);
 int salvar(Tarefa tarefas[], int total, int pos);
 int carregar(Tarefa tarefas[], int total, int pos);
+void clearBuffer();
+
+
+
